a734678073b84622bf8be8b6577358a0b9ed98b5
yadira <angela.escobedoq@istpargentina.edu.pe>
2026-07-13 20:01:56 -0500

    aqui se creo la estructura inicial de categoriaDao

--- backend/dao/categoria_dao.py
+++ backend/dao/categoria_dao.py
@@ -8,3 +8,11 @@ class CategoriaNoEncontradaError(Exception):
 class CategoriaDuplicadaError(Exception):
     def __init__(self, nombre):
         super().__init__(f"Categoría '{nombre}' ya existe")
+
+class CategoriaDAO:
+    def __init__(self):
+        self.__bd =[]
+        self.__cid = 1
+        self.__log = Logger()
+        self._cargar_categorias_base()
+        
