9bb2e76d42fb1cd285670b65447fe319542bff7a
AngieLiAl <angie.lziarsaburua@istpargentina.edu.pe>
2026-07-15 16:17:47 -0500

    Hice la función para registrar nuevos gastos en la base de datos y que se asigne automáticamente el ID del registro creado.

---
+++ backend/dao/gasto_dao.py
@@ -0,0 +1,29 @@
+from config.logger import Logger
+from config.base_datos import obtener_conexion
+from modelos.gasto import Gasto
+import sqlite3
+
+class GastoNoEncontradoError(Exception):
+    def __init__(self, gasto_id):
+        super().__init__(f"Gasto ID={gasto_id} no encontrado")
+
+class GastoDAO:
+    def __init__(self):
+        self.__log = Logger()
+
+def insertar(self, gasto):
+        conn = obtener_conexion()
+        cursor = conn.cursor()
+        cursor.execute(
+            """INSERT INTO gastos
+            (descripcion, monto, fecha, es_compartido, id_persona, id_categoria)
+            VALUES (?, ?, ?, ?, ?, ?)""",
+            (gasto.descripcion, gasto.monto, gasto.fecha,
+            1 if gasto.es_compartido else 0,
+            gasto.id_persona, gasto.id_categoria)
+        )
+        conn.commit()
+        gasto.id = cursor.lastrowid
+        conn.close()
+        self.__log.info(f"Gasto agregado: {gasto.descripcion} S/.{gasto.monto:.2f} (ID={gasto.id})")
+        return gasto
