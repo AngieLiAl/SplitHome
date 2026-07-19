a1b404169b9ea52e1c9a38a0e4f31e749922daf7
yadira <angela.escobedoq@istpargentina.edu.pe>
2026-07-18 22:09:09 -0500

    Agregué las importaciones de configuración y de los DAO

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+from config.base_datos import inicializar
+from config.sistema_config import SistemaConfig
+from config.logger import Logger
+from dao.persona_dao import PersonaDAO
+from dao.categoria_dao import CategoriaDAO
+from dao.gasto_dao import GastoDAO
+from dao.participacion_dao import ParticipacionDAO
+
