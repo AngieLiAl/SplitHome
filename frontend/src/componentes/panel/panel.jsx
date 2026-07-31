791ec9af8335ce6ddac662a0e67995afe6d4faad
yadira <angela.escobedoq@istpargentina.edu.pe>
2026-07-31 12:10:21 -0500

    Se agregó la función Panel y los estados iniciales para mostrar el total de personas, categorías, gastos y monto registrado.

--- frontend/src/componentes/panel/panel.jsx
+++ frontend/src/componentes/panel/panel.jsx
@@ -1,3 +1,12 @@
 import { FaUsers, FaTags, FaWallet, FaBalanceScale } from "react-icons/fa"
 import { Link } from "react-router-dom"
 import { useState } from "react"
+
+function Panel() {
+
+    const [totalPersonas]   = useState(2)
+    const [totalCategorias] = useState(7)
+    const [totalGastos]     = useState(5)
+    const [totalMonto] = useState(1624.00)
+
+    
